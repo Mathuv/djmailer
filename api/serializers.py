be34915eb02fc80a09c51b65471c12bab12d3b59
Medius Health <mediushealth@mediuss-mbp.vcso.customer>
2017-10-17 17:36:31 +1100

    Uses modelseriliazer instead of serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
 
+from .models import Subscriber
+
 
 class HelloWorldSerializer(serializers.Serializer):
     name = serializers.CharField(required=True, max_length=6)
     age = serializers.IntegerField(required=False, min_value=10, default=10)
 
 
-class SubscriberSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=50)
-    age = serializers.IntegerField()
-    email = serializers.EmailField()
+# class SubscriberSerializer(serializers.Serializer):
+#     name = serializers.CharField(max_length=50)
+#     age = serializers.IntegerField()
+#     email = serializers.EmailField()
+
+
+class SubscriberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscriber
+        fields = "__all__"
