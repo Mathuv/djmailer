a2fdf0ca66a3afac71b111d0347ffbba7ea0e317
Medius Health <mediushealth@mediuss-mbp.vcso.customer>
2017-10-17 12:38:38 +1100

    Converted to DRF based REST API

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
 
-from .views import hello_world
+# from .views import hello_world
+
+# for DRF
+from .views import HelloWorldView
 
 urlpatterns = [
-    url(r'^hello', hello_world, name="hello_world")
+    # url(r'^hello', hello_world, name="hello_world")
+    url(r'^hello', HelloWorldView.as_view(), name="hello_world")
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http.response import JsonResponse
 
+# For DRF
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 # Create your views here.
 def hello_world(request):
     return JsonResponse({"message": "hello world!"})
+
+class HelloWorldView(APIView):
+    def get(self, request):
+        return Response({"message": "Hello World DRF!"})
